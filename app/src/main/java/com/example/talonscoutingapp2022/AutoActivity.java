19c496829f47878c75c5ab8551f5921a1625a689
AdityaK2905 <60373662+AdityaK2905@users.noreply.github.com>
2022-02-28 00:09:15 -0600

    Added Home Screen and Auto Screen, w/ Rahul

--- app/src/main/java/com/example/talonscoutingapp2022/AutoActivity.java
+++ app/src/main/java/com/example/talonscoutingapp2022/AutoActivity.java
@@ -1,13 +1,26 @@
 package com.example.talonscoutingapp2022;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class AutoActivity extends AppCompatActivity {
-   boolean leftHangar = false;
+   boolean leftTarmac = false;
+    public int upperMade = 0;
+    public int lowerMade = 0;
+    public int upperMissed = 0;
+    public int lowerMissed = 0;
+
 
 
     @Override
@@ -15,16 +28,76 @@ public class AutoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auto_screen);
 
-        Button left_hangar_button = findViewById(R.id.leftHangarButton);
-        left_hangar_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                leftHangar = !leftHangar;
-                setContentView(R.layout.activity_auto_screen);
-            }
-        });
+
+    }
+
+
+    //You get points if you leave the hangar in auto phase, so button for that
+    public void leftHangerSuccessfully(View v){
+        leftTarmac = true;
+        ((TextView)findViewById(R.id.leftTarmacText)).setText("Left Tarmac: " + leftTarmac);
+    }
+
+    //undo button
+    public void leftHangerUndo(View v){
+        leftTarmac = false;
+        ((TextView)findViewById(R.id.leftTarmacText)).setText("Left Tarmac: " + leftTarmac);
+    }
 
 
 
+
+
+    //adds points to made upper or lower
+    public void upperMadePlus(View v){
+        upperMade+=1;
+        ((TextView)findViewById(R.id.upperMadeText2)).setText("Upper Made: " + upperMade);
+    }
+
+    public void lowerMadePlus(View v){
+        lowerMade++;
+        ((TextView) findViewById(R.id.lowerMadeText)).setText("Lower Made: "+lowerMade);
+    }
+
+    public void lowerGoalMiss(View v){
+        lowerMissed++;
+        ((TextView) findViewById(R.id.lowerMissedText)).setText("Lower Missed: "+ lowerMissed);
+    }
+
+    public void upperGoalMiss(View v){
+        upperMissed++;
+        ((TextView) findViewById(R.id.upperMissedText)).setText("Upper Missed: "+upperMissed);
+    }
+
+    public void undoUpperMade(View v){
+        if (upperMade == 0) {
+            upperMade=0;
+        }
+        upperMade--;
+        ((TextView)findViewById(R.id.upperMadeText2)).setText("Upper Made: " + upperMade);
+    }
+
+    public void undoUpperMiss(View v){
+        if (upperMissed == 0) {
+            return;
+        }
+        upperMissed--;
+        ((TextView) findViewById(R.id.upperMissedText)).setText("Upper Missed: "+upperMissed);
+    }
+
+    public void undoLowerMiss(View v){
+        if (lowerMissed == 0) {
+            return;
+        }
+        lowerMissed--;
+        ((TextView) findViewById(R.id.lowerMissedText)).setText("Lower Missed: "+ lowerMissed);
+    }
+
+    public void undoLowerMade(View v){
+        if (lowerMade == 0) {
+            return;
+        }
+        lowerMade--;
+        ((TextView) findViewById(R.id.lowerMadeText)).setText("Lower Made: "+lowerMade);
     }
 }
--- app/src/main/java/com/example/talonscoutingapp2022/MainActivity.java
+++ app/src/main/java/com/example/talonscoutingapp2022/MainActivity.java
@@ -5,9 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String teamNum;
+    String matchNum;
+    String driverStation;
+    String scouter;
+    String teamColor = "Red";
 
 
 
@@ -17,15 +29,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Button start_game_button = findViewById(R.id.startGameButton);
-        /*
-        start_game_button.setOnClickListener(new View.OnClickListener() {
+        //Scouter Drop Down List
+        Spinner scoutList = (Spinner) findViewById(R.id.scouterList);
+        ArrayAdapter<String> scoutListAdapter = new ArrayAdapter<>
+                (MainActivity.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.scout_names));
+        scoutListAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        scoutList.setAdapter(scoutListAdapter);
+
+        //team color selector
+        Switch colorSwitch = (Switch) findViewById(R.id.teamSwitch);
+        colorSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, AutoActivity.class));
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if(isChecked ==true){
+                    teamColor = "Blue";
+                    colorSwitch.setText(teamColor);
+                }
+                if(isChecked == false){
+                    teamColor = "Red";
+                    colorSwitch.setText(teamColor);
+                }
             }
         });
-*/
+
+        //set team number and match number
+        EditText teamNumber =  (EditText) findViewById(R.id.editTeamNumber);
+        EditText matchNumber =  (EditText) findViewById(R.id.editMatchNumber);
+
+        teamNum = teamNumber.getText().toString();
+        matchNum = matchNumber.getText().toString();
+
 
 
     }
